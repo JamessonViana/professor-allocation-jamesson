2df58794fdc15a29547a623d77d457096cd04810
Jamesson Viana <jamesson1925@hotmail.com>
2024-02-24 14:30:15 -0300

    add Project

---
+++ src/main/java/com/projet/professor/allocation/jamesson/ProfessorAllocationJamessonApplication.java
@@ -0,0 +1,13 @@
+package com.projet.professor.allocation.jamesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationJamessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationJamessonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet/professor/allocation/jamesson/ProfessorAllocationJamessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.professor.allocation.jamesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationJamessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
